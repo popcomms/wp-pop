202650f941699cf06e7dddf89dd54e4f55fef35c
Jon Snow <47143928+therealJonSnow@users.noreply.github.com>
2021-05-10 14:34:45 +0100

    Formssss

--- static/main.js
+++ static/main.js
@@ -108,7 +108,9 @@ document.addEventListener("DOMContentLoaded", function() {
         this.step += 1
       },
       prevStep () {
+        console.log('prev')
         if (this.step > 0 && this.tl.paused() === true) {
+          console.log('prev active')
           this.tl.reverse()
           this.step -= 1
         }
@@ -128,63 +130,54 @@ document.addEventListener("DOMContentLoaded", function() {
           this.nextStep()
         }
       },
-      revealInput(target) {
-        const swipes = target.getElementsByClassName('input-swipe')
-        const inputs = target.getElementsByClassName('input-box')
-
+      revealInput(step) {
+        const borders = step.getElementsByClassName('input-border')
+        const guides = step.getElementsByClassName('input-guide')
+        const inputs = step.querySelectorAll('.input-content')
+        
         if (this.tl.reversed()) {
-          this.hideInputAnim(inputs, swipes)
+          this.hideInputAnim(step, inputs, borders, guides)
         } else {
-          this.revealInputAnim(inputs, swipes)
+          this.revealInputAnim(step, inputs, borders, guides)
         }
       },
-      hideInput(target) {
-        const swipes = target.getElementsByClassName('input-swipe')
-        const inputs = target.getElementsByClassName('input-box')
+      hideInput(step) {
+        const borders = step.getElementsByClassName('input-border')
+        const guides = step.getElementsByClassName('input-guide')
+        const inputs = step.querySelectorAll('.input-content')
+        
         if (this.tl.reversed()) {
-          this.revealInputAnim(inputs, swipes)
+          this.revealInputAnim(step, inputs, borders, guides)
         } else {
-          this.hideInputAnim(inputs, swipes)
+          this.hideInputAnim(step, inputs, borders, guides)
         }
       },
-      hideInputAnim (inputs, swipes) {
+      hideInputAnim (step, inputs, borders, guides) {
         gsap.timeline({
         })
-        .to(swipes, {duration: 0.15, top: 0 + '%'})
-        .to(inputs, {duration: 0.15, opacity: 0})
-        .to(swipes, {duration: 0.15, top: 100 + '%'})
-        .set(swipes, {top: -100 + '%'})
+        .to(borders, {duration: 0.5, width: 0 + '%'})
+        .to(inputs, {duration: 0.5, translateY: -100 + '%', opacity: 1,  ease: "power3.in"}, "-=0.5")
+        .to(guides, {duration: 0.3, opacity: 0,  ease: "power3.in"}, "-=0.3")
+        .set(inputs, {translateY: 100 + '%'})
+        .set(step, {pointerEvents: 'none'})
       },
-      revealInputAnim (inputs, swipes) {
+      revealInputAnim (step, inputs, borders, guides) {
         gsap.timeline({
         })
-        .to(swipes, {duration: 0.15, top: 0 + '%'})
-        .to(inputs, {duration: 0.15, opacity: 1})
-        .to(swipes, {duration: 0.15, top: 100 + '%'})
-        .set(swipes, {top: -100 + '%'})
-      }
+        .to(borders, {duration: 0.5, width: 100 + '%'})
+        .to(inputs, {duration: 0.5, translateY: 0 + '%', opacity: 1, ease: "power3.out"}, "-=0.5")
+        .to(guides, {duration: 0.3, opacity: 1,  ease: "power3.in"}, "-=0.3")
+        .set(step,{ pointerEvents: 'all'}, "-=0.01")
+      },
     },
     mounted () {
       gsap.set('.steps', {pointerEvents: 'none'})
       
-      this.tl = gsap
-      .timeline({ paused: true })
-      .to(
-        ".downloads-cover",
-        {
-          left: 0,
-          duration: 0.4
-        }
-      )
+      this.tl = gsap.timeline({ paused: true })
+      // Show form
+      .to(".downloads-cover",{left: 0, duration: 0.4})
       .set('.steps',{ pointerEvents: 'all' })
-      .to(
-        [".download-steps", ".download-back"],
-        {
-          opacity: 1,
-          duration: 0.3
-        }
-      )
-
+      .to([".download-steps", ".download-back"], {opacity: 1,duration: 0.3})
       .fromTo(
         this.$refs.step_1,
         {
@@ -198,28 +191,133 @@ document.addEventListener("DOMContentLoaded", function() {
         }
       )
       .addPause()
-      
+      // Change step 1 -> 2
       .to(this.$refs.step_1, {duration: 0.3, opacity: 0, pointerEvents: 'none'})
-      .call( this.revealInput, [this.$refs.step_2], "+=0.1" )
-      .set(this.$refs.step_2,{ pointerEvents: 'all'}, "-=0.01")
-      .addPause("+=0.5")
-      .call( this.hideInput, [this.$refs.step_2], "+=0.01" )
-      
-      .to(this.$refs.step_2, {duration: 0.3, pointerEvents: 'none'})
-      .set(this.$refs.step_3,{ pointerEvents: 'all'})
-      .call( this.revealInput, [this.$refs.step_3], "-=0.01" )
-      .addPause("+=0.5")
-      .call( this.hideInput, [this.$refs.step_3], "+=0.01" )
-      
-      .to(this.$refs.step_3, {duration: 0.3, pointerEvents: 'none'})
-      .set(this.$refs.step_4,{ pointerEvents: 'all'})
-      .call( this.revealInput, [this.$refs.step_4], "-=0.01" )
-      .addPause("+=0.5")
-      .call( this.hideInput, [this.$refs.step_4], "+=0.01" )
-
-      .to(this.$refs.step_4, {duration: 0.3, pointerEvents: 'none'})
+      .call( this.revealInput, [this.$refs.step_2], "+=0.6" )
+      .addPause("+=0.2")
+      // Change step 2 -> 3
+      .call( this.hideInput, [this.$refs.step_2], "+=0.1" )
+      .call( this.revealInput, [this.$refs.step_3], "+=0.6" )
+      .addPause("+=0.2")
+      // Change step 3 -> 4
+      .call( this.hideInput, [this.$refs.step_3], "+=0.1" )
+      .call( this.revealInput, [this.$refs.step_4], "+=0.6" )
+      .addPause("+=0.2")
+      // Change step 4 -> 5
+      .call( this.hideInput, [this.$refs.step_4], "+=0.1" )
       .fromTo(
         this.$refs.step_5,
+        {
+          opacity: 0,
+          pointerEvents: 'none'
+        },
+        {
+          pointerEvents: 'all',
+          opacity: 1,
+          duration: 0.3
+        },
+        "+=0.6"
+      )
+      // Stop before end so can reverse
+      .addPause()
+    }
+  })
+  
+
+  Vue.component('contact-form', {
+    data () {
+      return {
+        step: 0,
+        enquiry: '', 
+        email: '',
+        name: '',
+        showInput: 'email'
+      }
+    },
+    computed: {
+      activeForm () {
+        if (this.showInput === 'email') {
+          return 'text-pop-yellow'
+        } else {
+          return 'text-pop-white'
+        }
+      }
+    },
+    methods: {
+      nextStepContact () {
+        this.contactTl.play();
+        this.step += 1
+      },
+      prevStepContact () {
+        if (this.step > 0 && this.contactTl.paused() === true) {
+          this.contactTl.reverse()
+          this.step -= 1
+        }
+      },
+      validateEmail (e) {
+        if (e.keyCode === 13 || e.type === 'click') {
+          this.nextStepContact()
+        }
+      },
+      validateName (e) {
+        if (e.keyCode === 13 || e.type === 'click') {
+          this.nextStepContact()
+        }
+      },
+      revealInput(step) {
+        const borders = step.getElementsByClassName('input-border')
+        const guides = step.getElementsByClassName('input-guide')
+        const inputs = step.querySelectorAll('.input-content')
+        const text = step.querySelectorAll('h4')
+        
+        if (this.contactTl.reversed()) {
+          this.hideInputAnim(step, inputs, borders, guides, text)
+        } else {
+          this.revealInputAnim(step, inputs, borders, guides, text)
+        }
+      },
+      hideInput(step) {
+        const borders = step.getElementsByClassName('input-border')
+        const guides = step.getElementsByClassName('input-guide')
+        const inputs = step.querySelectorAll('.input-content')
+        const text = step.querySelectorAll('h4')
+        
+        if (this.contactTl.reversed()) {
+          this.revealInputAnim(step, inputs, borders, guides, text)
+        } else {
+          this.hideInputAnim(step, inputs, borders, guides, text)
+        }
+      },
+      hideInputAnim (step, inputs, borders, guides, text) {
+        gsap.timeline({
+        })
+        .to(borders, {duration: 0.5, width: 0 + '%'})
+        .to(text, {duration: 0.5, opacity: 0}, "-0.5")
+        .to(inputs, {duration: 0.5, translateY: -100 + '%', opacity: 1,  ease: "power3.in"}, "-=0.5")
+        .to(guides, {duration: 0.3, opacity: 0,  ease: "power3.in"}, "-=0.3")
+        .set(inputs, {translateY: 100 + '%'})
+        .set(step, {pointerEvents: 'none'})
+      },
+      revealInputAnim (step, inputs, borders, guides, text) {
+        gsap.timeline({
+        })
+        .to(borders, {duration: 0.5, width: 100 + '%'})
+        .to(text, {duration: 0.5, opacity: 1}, "-0.5")
+        .to(inputs, {duration: 0.5, translateY: 0 + '%', opacity: 1, ease: "power3.out"}, "-=0.5")
+        .to(guides, {duration: 0.3, opacity: 1,  ease: "power3.in"}, "-=0.3")
+        .set(step,{ pointerEvents: 'all'}, "-=0.01")
+      },
+    },
+    mounted () {
+      gsap.set(this.$refs.steps, {pointerEvents: 'none'})
+      
+      this.contactTl = gsap.timeline({ paused: true })
+      // Show form
+      .to(this.$refs.form_bg,{opacity: 1, duration: 0.4})
+      .set(this.$refs.steps,{ pointerEvents: 'all' })
+      .to([".contact-steps", ".contact-back"], {opacity: 1,duration: 0.3})
+      .fromTo(
+        this.$refs.step_1,
         {
           opacity: 0,
           pointerEvents: 'none'
@@ -231,10 +329,38 @@ document.addEventListener("DOMContentLoaded", function() {
         }
       )
       .addPause()
-
+      // Change step 1 -> 2
+      .to(this.$refs.step_1, {duration: 0.3, opacity: 0, pointerEvents: 'none'})
+      .to(this.$refs.step_2.querySelectorAll('h4'), {duration: 0.3, opacity: 1})
+      .call( this.revealInput, [this.$refs.email_input], "-=0.3" )
+      .addPause("+=0.2")
+      // switch inputs
+      .call( this.hideInput, [this.$refs.email_input], "+=0.1" )
+      .to(this.$refs.email_text, {duration: 0.3, color: '#FFFFFF'})
+      .call( this.revealInput, [this.$refs.name_input], "+=0.3" )
+      .to(this.$refs.name_text, {duration: 0.3, color: '#FFFF99'})
+      .addPause("+=0.2")
+      // Change step 2 -> 3
+      .to(this.$refs.step_2.querySelectorAll('h4'), {duration: 0.3, opacity: 0})
+      .call( this.hideInput, [this.$refs.name_input], "-=0.29" )
+      .fromTo(
+        this.$refs.step_3,
+        {
+          opacity: 0,
+          pointerEvents: 'none'
+        },
+        {
+          pointerEvents: 'all',
+          opacity: 1,
+          duration: 0.3
+        },
+        "+=0.6"
+      )
+      // Stop before end so can reverse
+      .addPause()
     }
   })
-  
+
   new Vue({
     el: document.getElementById('site-wrapper')
   })
