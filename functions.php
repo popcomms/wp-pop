eba1bec4a40d909baf206f5e2b3d719747a7bf16
simon-hayden <simon@fraserhayden.com>
2021-06-09 17:13:34 +0100

    Add Client Taxonomy

--- functions.php
+++ functions.php
@@ -174,7 +174,7 @@ add_filter('wp_editor_set_quality', function($arg) { return 100; } );
 function wpb_add_google_fonts() {
 	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap', false );
 }
-	
+
 add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
 
 
@@ -301,7 +301,7 @@ add_theme_support( 'editor-color-palette', array(
 add_action('acf/init', 'my_acf_init');
 
 function my_acf_init() {
-	
+
 	// check function exists
 	if ( ! function_exists( 'acf_register_block' ) ) {
 			return;
@@ -348,7 +348,7 @@ function my_acf_init() {
 	// 	'icon'				=> 'admin-comments',
 	// 	'keywords'			=> array( 'section' ),
 	// ));
-	
+
 	// acf_register_block(array(
 	// 	'name'				=> 'text-block',
 	// 	'title'				=> __('Textblock'),
@@ -443,37 +443,79 @@ function my_acf_init() {
 }
 
 // Register Custom Post Types
+
 function custom_case_study_post_type() {
 	register_post_type('case-studies',
-			array(
-					'labels'      => array(
-							'name'          => __('Case Studies'),
-							'singular_name' => __('Case Study'),
-					),
-					'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
-					'public' => true,
-					'has_archive' => true,
-					'show_in_rest' => true
-			)
+    array(
+      'labels'      => array(
+        'name'          => __('Case Studies'),
+        'singular_name' => __('Case Study'),
+      ),
+      'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
+      'public' => true,
+      'has_archive' => true,
+      'show_in_rest' => true
+    )
 	);
 }
 add_action('init', 'custom_case_study_post_type');
 
 function custom_portfolio_post_type() {
 	register_post_type('portfolio',
-			array(
-					'labels'      => array(
-							'name'          => __('Portfolio'),
-							'singular_name' => __('Portfolio Item'),
-					),
-					'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
-					'public' => true,
-					'has_archive' => true,
-					'show_in_rest' => true
-			)
+    array(
+      'labels'      => array(
+        'name'          => __('Portfolio'),
+        'singular_name' => __('Portfolio Item'),
+      ),
+      'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
+      'public' => true,
+      'has_archive' => true,
+      'show_in_rest' => true
+    )
 	);
 }
 add_action('init', 'custom_portfolio_post_type');
+
+// Client Taxonomy
+
+// Register Client Taxonomy
+function custom_client_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Clients', 'Taxonomy General Name', 'wp-pop' ),
+		'singular_name'              => _x( 'Client', 'Taxonomy Singular Name', 'wp-pop' ),
+		'menu_name'                  => __( 'Clients', 'wp-pop' ),
+		'all_items'                  => __( 'All Clients', 'wp-pop' ),
+		'parent_item'                => __( 'Parent Client', 'wp-pop' ),
+		'parent_item_colon'          => __( 'Parent Client:', 'wp-pop' ),
+		'new_item_name'              => __( 'New Client Name', 'wp-pop' ),
+		'add_new_item'               => __( 'Add New Client', 'wp-pop' ),
+		'edit_item'                  => __( 'Edit Client', 'wp-pop' ),
+		'update_item'                => __( 'Update Client', 'wp-pop' ),
+		'view_item'                  => __( 'View Client', 'wp-pop' ),
+		'separate_items_with_commas' => __( 'Separate Clients with commas', 'wp-pop' ),
+		'add_or_remove_items'        => __( 'Add or remove Clients', 'wp-pop' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'wp-pop' ),
+		'popular_items'              => __( 'Popular Clients', 'wp-pop' ),
+		'search_items'               => __( 'Search Clients', 'wp-pop' ),
+		'not_found'                  => __( 'Not Found', 'wp-pop' ),
+		'no_terms'                   => __( 'No Clients', 'wp-pop' ),
+		'items_list'                 => __( 'Clients list', 'wp-pop' ),
+		'items_list_navigation'      => __( 'Clients list navigation', 'wp-pop' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => false,
+	);
+	register_taxonomy( 'client', array( 'case-studies' ), $args );
+
+}
+add_action( 'init', 'custom_client_taxonomy', 0 );
 
 add_action( 'show_user_profile', 'extra_user_profile_fields' );
 add_action( 'edit_user_profile', 'extra_user_profile_fields' );
