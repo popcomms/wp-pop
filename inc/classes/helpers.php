ca4141df1b2bf7b97e337e67a3e21b2aad7874d9
Luke Taylor <hello@avalanchedigital.co.uk>
2024-04-19 09:51:14 +0100

    video dialogs

--- inc/classes/helpers.php
+++ inc/classes/helpers.php
@@ -32,6 +32,90 @@
       error_log(print_r($data, TRUE));
   
     }
+    
+    public static function parse_iframe_video($iframe) {
+      
+      $output = [
+        'type' => '',
+        'id' => '',
+        'url' => '',
+        'link' => '',
+        'platform_name' => ''
+      ];
+    
+      preg_match('/src="(.+?)"/', $iframe, $matches);
+      $video_src = $matches[1];
+    
+      $parse = parse_url($video_src);
+    
+      $video_type = '';
+      $video_id = '';
+    
+      if ($parse['host'] == 'youtu.be') {
+    
+        $video_type = 'youtube';
+        $video_id = ltrim($parse['path'], '/');
+    
+      }
+    
+      if (
+        $parse['host'] == 'youtube.com' ||
+        $parse['host'] == 'www.youtube.com'
+      ) {
+    
+        $video_type = 'youtube';
+        parse_str($parse['query'], $result);
+    
+        if (isset($result['v'])) { $video_id = $result['v']; }
+        if (!empty($feature)) { $video_id = end(explode('v=', $parse['query'])); }
+    
+        if (strpos($parse['path'], 'embed') == 1) {
+    
+          $video_id = explode('/', $parse['path']);
+          $video_id = end($video_id);
+    
+        }
+    
+      }
+    
+      if (
+        $parse['host'] == 'vimeo.com' ||
+        $parse['host'] == 'www.vimeo.com' ||
+        $parse['host'] == 'player.vimeo.com'
+      ) {
+    
+        $video_type = 'vimeo';
+        $video_id = ltrim($parse['path'], '/');
+        $video_id = preg_replace('/\D/', '', $video_id);
+    
+      }
+    
+      if (!empty($video_type)) {
+        
+        $output['type'] = $video_type;
+        $output['id'] = $video_id;
+    
+        if ($video_type == 'vimeo') {
+          
+          $output['url'] = '//player.vimeo.com/video/' . $video_id . '?autoplay=0';
+          $output['link'] = 'https://vimeo.com/' . $video_id;
+          $output['platform_name'] = 'Vimeo';
+          return $output;
+    
+        } else if ($video_type == 'youtube') {
+          
+          $output['url'] = '//www.youtube-nocookie.com/embed/' . $video_id . '?autoplay=0&rel=0&showinfo=0';
+          $output['link'] = 'https://www.youtube.com/watch?v=' . $video_id;
+          $output['platform_name'] = 'YouTube';
+          return $output;
+    
+        }
+    
+      }
+      
+      return $output;
+    
+    }
   
   }
   
