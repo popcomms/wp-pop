7a2bc1bbf68acd9d645a87639db1c4d2c1be2094
Luke Taylor <hello@avalanchedigital.co.uk>
2024-06-04 09:30:28 +0100

    switch to gulp build for modular css / js

---
+++ frontend/src/js/bundles/main.js
@@ -0,0 +1,5 @@
+//=require "../lib/_body-scroll-lock.js"
+
+//=require "../modules/_dialog.js"
+//=require "../modules/_video-banner.js"
+//=require "../modules/_video.js"
---
+++ frontend/src/js/modules/_video-banner.js
@@ -0,0 +1,50 @@
+(function() {
+  
+  var elements = document.querySelectorAll('[data-video-banner]');
+  
+  if (!elements.length) return;
+  
+  return;
+  
+  setHeights();
+  window.addEventListener('resize', debounce(setHeights, 500));
+  
+  function setHeights() {
+    
+    for (var i = 0; i < elements.length; i++) {
+      (function (i) {
+        
+        elements[i].removeAttribute('style');
+        elements[i].style.height = elements[i].offsetHeight + 'px';
+        elements[i].style.minHeight = elements[i].offsetHeight + 'px';
+    
+      }).call(this, i);
+    }
+    
+  }
+  
+  function debounce(func, wait, immediate) {
+  
+    var timeout;
+  
+    return function executedFunction() {
+      var context = this;
+      var args = arguments;
+  
+      var later = function() {
+        timeout = null;
+        if (!immediate) { func.apply(context, args); }
+      };
+  
+      var callNow = immediate && !timeout;
+  
+      clearTimeout(timeout);
+  
+      timeout = setTimeout(later, wait);
+  
+      if (callNow) { func.apply(context, args); }
+    };
+  
+  }
+  
+})();
